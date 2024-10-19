1dfc3d23c196b2cafdde8e30dd94392756ddddd2
dustom <dusek.tomas@icloud.com>
2024-10-19 10:50:14 +0200

    Initial Commit

---
+++ AthleteWeather/AthleteWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AthleteWeatherApp.swift
+//  AthleteWeather
+//
+//  Created by Tomáš Dušek on 19.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct AthleteWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
