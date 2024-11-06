2547daeea59e684f8069fd6ffe10e510be94e516
dustom <dusek.tomas@icloud.com>
2024-11-06 21:23:55 +0100

    icon change + search suggestions  implementation

---
+++ AthleteWeather/Models/ConstantsDummy.swift
@@ -0,0 +1,8 @@
+//
+//  ConstantsDummy.swift
+//  AthleteWeather
+//
+//  Created by Tomáš Dušek on 06.11.2024.
+//
+
+import Foundation
---
+++ AthleteWeather/Models/CurrentWeather/LocationSearchService.swift
@@ -0,0 +1,31 @@
+//
+//  LocationSearchSuggest.swift
+//  AthleteWeather
+//
+//  Created by Tomáš Dušek on 06.11.2024.
+//
+
+import Foundation
+import SwiftUI
+import MapKit
+import Combine
+
+class LocationSearchService: NSObject, ObservableObject, MKLocalSearchCompleterDelegate {
+    @Published var searchQuery = ""
+    var completer: MKLocalSearchCompleter
+    @Published var completions: [MKLocalSearchCompletion] = []
+    var cancellable: AnyCancellable?
+    
+    override init() {
+        completer = MKLocalSearchCompleter()
+        super.init()
+        cancellable = $searchQuery.assign(to: \.queryFragment, on: self.completer)
+        completer.delegate = self
+    }
+    
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        self.completions = completer.results
+    }
+}
+
+extension MKLocalSearchCompletion: @retroactive Identifiable {}
