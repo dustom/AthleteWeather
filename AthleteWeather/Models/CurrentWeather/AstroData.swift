b742344d9639f368fd1a1636cd96792a72815e1e
dustom <dusek.tomas@icloud.com>
2024-11-09 11:09:32 +0100

    new API integration, WeatherSEttingsView slight redising

---
+++ AthleteWeather/Models/CurrentWeather/AstroData.swift
@@ -0,0 +1,8 @@
+//
+//  AstroData.swift
+//  AthleteWeather
+//
+//  Created by Tomáš Dušek on 09.11.2024.
+//
+
+import Foundation
---
+++ AthleteWeather/Models/CurrentWeather/WeatherCondition.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherCondition.swift
+//  AthleteWeather
+//
+//  Created by Tomáš Dušek on 09.11.2024.
+//
+
+import Foundation
